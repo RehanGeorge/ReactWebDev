2fd558b4e4ced5cfe5c5cae11f8c765ba1af2447
Rehan George <rehan.george@gmail.com>
2024-01-31 20:08:51 +0530

    Implement Conditional Rendering of Form

--- conditional-rendering/src/components/App.jsx
+++ conditional-rendering/src/components/App.jsx
@@ -1,16 +1,12 @@
 import React from "react";
+import Form from "./Form";
 
-let isLoggedIn = true;
+var userIsRegistered = false;
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      <Form user={userIsRegistered}/>
     </div>
   );
 }
