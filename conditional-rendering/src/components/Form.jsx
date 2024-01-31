5c3e88d996b3ba3cddc14b1fce85f7aee10939d6
Rehan George <rehan.george@gmail.com>
2024-01-31 20:12:27 +0530

    Correct confirm password view

--- conditional-rendering/src/components/Form.jsx
+++ conditional-rendering/src/components/Form.jsx
@@ -5,7 +5,7 @@ function Form(props) {
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      {props.user && <input type="password" placeholder="Confirm Password" />}
+      {!props.user && <input type="password" placeholder="Confirm Password" />}
       <button type="submit">{props.user ? "Login" : "Register"}</button>
     </form>
   );
