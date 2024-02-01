c82e307125a79e12ebe4b9318c3bc28ec9d687e7
Rehan George <rehan.george@gmail.com>
2024-02-01 22:12:40 +0530

    Implement mouse over and mouse out on submit

--- event-app/src/components/App.jsx
+++ event-app/src/components/App.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
+  const button = document.querySelector("button");
+
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleHover() {
+    setMouseOver(true);
+  }
+
+  function handleOut() {
+    setMouseOver(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button style={{ backgroundColor: isMouseOver ? "black" : "white" }} onClick={handleClick} onMouseOver={handleHover} onMouseOut={handleOut}>Submit</button>
     </div>
   );
 }
