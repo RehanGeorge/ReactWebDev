fb860f6b45d5baeece09cf61404f75cebe958eaa
Rehan George <rehan.george@gmail.com>
2024-02-02 19:45:58 +0530

    Implement state managed form

--- change-app/src/components/App.jsx
+++ change-app/src/components/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
+    const [name, setName] = useState("");
+    const [headingText, setHeadingText] = useState("Hello");
+
+    function handleChange(event) {
+        setName(event.target.value);
+    }
+
+    function handleClick() {
+        setHeadingText(`Hello ${name}`);
+    }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>{headingText}</h1>
+      <input onChange={handleChange} type="text" placeholder="What's your name?" value={name}/>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
