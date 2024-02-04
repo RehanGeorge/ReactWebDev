cf258f5db3f26e7015bef290b54692ead72c6ddf
Rehan George <rehan.george@gmail.com>
2024-02-04 17:04:02 +0530

    Implement component based functionality

--- component-tree/src/components/App.jsx
+++ component-tree/src/components/App.jsx
@@ -1,45 +1,40 @@
 import React, { useState } from "react";
-import ListItem from "./ListItem";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
-  const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
-
-  function addItem() {
+  function addItem(inputText) {
     setItems(prevItems => {
       return [...prevItems, inputText];
     });
-    setInputText("");
   }
 
   function deleteItem(id) {
     setItems(prevItems => {
-        return prevItems.filter((item, index) => {
-            return index !== id});
-        });
-    }
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
 
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea onAdd={addItem}/>
       <div>
         <ul>
-            {items.map((item, index) => {
-                return <ListItem key={index} id={index} item={item} onChecked={deleteItem}/>;
-            })}
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
         </ul>
       </div>
     </div>
--- component-tree/src/components/ListItem.jsx
+++
@@ -1,7 +0,0 @@
-export default function ListItem(props) {
-    return (
-    <div onClick={() => {props.onChecked(props.id)}}>
-        <li>{props.item}</li>
-    </div>
-    )
-}
